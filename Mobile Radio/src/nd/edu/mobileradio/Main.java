5bd64dea5a828cc07b45d756c43dff58f8c9e807
ekloswic@nd.edu <ekloswic@nd.edu@2188ca64-c304-c4c3-6ab6-9ce805551fbb>
2012-10-07 19:07:03 +0000

    First Parse implementation, added Parse library

--- Mobile Radio/src/nd/edu/mobileradio/Main.java
+++ Mobile Radio/src/nd/edu/mobileradio/Main.java
@@ -1,5 +1,13 @@
 package nd.edu.mobileradio;
 
+/*import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.PushService;
+import com.parse.SignUpCallback;*/
+import com.parse.*;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -20,13 +28,17 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Initialize the Parse thingy
+        Parse.initialize(this, "jfji0P8sAF26LoXmYVuoMCA86g0XG32FYtA3uPMn", "3xGdnBcGpHAr7XS21JPn3XlEl9Xj1mCKqXOBTlc0");
+        
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
+    }*/
     
     public void onStart() {
 		super.onStart();
@@ -35,12 +47,37 @@ public class Main extends Activity {
         listenButton = (Button) findViewById(R.id.listenButton);
         listenButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
+
+            	ParseUser user = new ParseUser();
+            	user.setUsername("E-fresh");
+            	user.setPassword("The Ancient Onez");
+            	user.setEmail("ekloswic@nd.edu");
+            	
+            	// other fields can be set just like with ParseObject
+            	user.put("phone", "789-456-1230");
+            	 
+            	user.signUpInBackground(new SignUpCallback() {
+            	  public void done(ParseException e) {
+            	    if (e == null) {
+            	    	listenButton.setText("Success!");
+            	    } else {
+            	    	listenButton.setText("FAILURE!!!");
+            	    }
+            	  }
+            	});
+            	
+                /*ParseObject testObject = new ParseObject("Bacon");
+                testObject.put("lettuce", "test");
+                testObject.saveInBackground();
+            	
+                
             	Intent myIntent = new Intent(v.getContext(), Listen.class);
-                startActivityForResult(myIntent, 0);
+                startActivityForResult(myIntent, 0);*/
+
+              
             }
         });
         
-        // Initialize the buttons with listeners for click events
         newsButton = (Button) findViewById(R.id.newsButton);
         newsButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -49,7 +86,6 @@ public class Main extends Activity {
             }
         });
         
-        // Initialize the buttons with listeners for click events
         contestButton = (Button) findViewById(R.id.contestButton);
         contestButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -58,7 +94,6 @@ public class Main extends Activity {
             }
         });
         
-        // Initialize the buttons with listeners for click events
         settingsButton = (Button) findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -67,4 +102,5 @@ public class Main extends Activity {
             }
         });
 	}
+    
 }
